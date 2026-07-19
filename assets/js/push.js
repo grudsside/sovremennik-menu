d2282922c40d397946d5f2bdf7aaf0d01fdd4df4
grudsside <uhalovgrigorij40731@gmail.com>
2026-07-19 09:35:31 +0300

    Исправить мобильные задачи и обновить интерфейс главной (#19)
    Мобильные задачи, ближайшие события, счетчик ревизий, уведомления и общая кнопка обновления сводки.

--- assets/js/push.js
+++ assets/js/push.js
@@ -1,7 +1,9 @@
 /* Современник script loader: preserve push logic, then add the interface shell. */
 /* push-legacy.js keeps loading assets/js/employee-status.js and all existing push handlers. */
 document.write('<link rel="stylesheet" href="assets/css/interface-v3-hotfix.css?v=20260718-1">');
+document.write('<link rel="stylesheet" href="assets/css/interface-followup.css?v=20260719-1">');
 document.write('<script src="assets/js/push-legacy.js?v=20260718"><\/script>');
 document.write('<script src="assets/js/interface-redesign.js?v=20260718"><\/script>');
 document.write('<script src="assets/js/interface-v3.js?v=20260718-3"><\/script>');
 document.write('<script src="assets/js/tasks-hotfix.js?v=20260718-1"><\/script>');
+document.write('<script src="assets/js/interface-followup.js?v=20260719-1"><\/script>');
