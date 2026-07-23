50aa66e8807c5ae5c37491f92406bbb70e8df264
grudsside <uhalovgrigorij40731@gmail.com>
2026-07-23 19:58:21 +0300

    Bust shift handoff assets for admin lifecycle

--- assets/js/push.js
+++ assets/js/push.js
@@ -12,7 +12,7 @@ document.write('<link rel="stylesheet" href="assets/css/mobile-photo-expand.css?
 document.write('<link rel="stylesheet" href="assets/css/checklist-photo-reports.css?v=20260722-1">');
 document.write('<link rel="stylesheet" href="assets/css/checklist-photo-viewer-fit.css?v=20260722-1">');
 document.write('<link rel="stylesheet" href="assets/css/offline-reliability.css?v=20260723-1">');
-document.write('<link rel="stylesheet" href="assets/css/shift-handoff.css?v=20260723-3">');
+document.write('<link rel="stylesheet" href="assets/css/shift-handoff.css?v=20260723-4">');
 document.write('<script src="assets/js/push-legacy.js?v=20260718"><\/script>');
 document.write('<script src="assets/js/interface-redesign.js?v=20260720-2"><\/script>');
 document.write('<script src="assets/js/tasks-v2.js?v=20260720-1"><\/script>');
@@ -32,4 +32,4 @@ document.write('<script src="assets/js/checklist-photo-reports.js?v=20260722-1">
 document.write('<script src="assets/js/offline-core.js?v=20260723-1"><\/script>');
 document.write('<script src="assets/js/offline-sync.js?v=20260723-1"><\/script>');
 document.write('<script src="assets/js/shift-handoff-core.js?v=20260723-1"><\/script>');
-document.write('<script src="assets/js/shift-handoff.js?v=20260723-3"><\/script>');
+document.write('<script src="assets/js/shift-handoff.js?v=20260723-4"><\/script>');
