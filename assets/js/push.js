de15f23e4da7d38fad05e5267f1c403e7a049eff
grudsside <uhalovgrigorij40731@gmail.com>
2026-07-25 18:40:32 +0300

    Подключить координатор Контроля с защитой офлайн-черновиков

--- assets/js/push.js
+++ assets/js/push.js
@@ -53,4 +53,4 @@ document.write('<script src="assets/js/home-layout-v4.js?v=20260724-3"><\/script
 document.write('<script src="assets/js/checklist-review-observer-guard.js?v=20260724-1"><\/script>');
 document.write('<script src="assets/js/checklist-review-tools.js?v=20260725-2"><\/script>');
 document.write('<script src="assets/js/checklist-photo-draft-fix.js?v=20260725-2"><\/script>');
-document.write('<script src="assets/js/control-section-stability.js?v=20260725-1"><\/script>');
+document.write('<script src="assets/js/control-section-stability-v2.js?v=20260725-2"><\/script>');
